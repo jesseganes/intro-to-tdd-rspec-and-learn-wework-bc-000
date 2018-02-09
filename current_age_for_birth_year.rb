614eee2e3cfd63a5dadc304f1011668de2af385d
Jesse Ganes <jesse.ganes@wework.com>
2018-02-09 00:34:57 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,7 +1,7 @@
-year = 1984
+# year = 1984
 
-def current_age_for_birth_year(bananas)
-  puts bananas
-end
+# def current_age_for_birth_year(bananas)
+#   puts bananas
+# end
 
-age_of_person = 2003 - current_age_for_birth_year(year)
+# age_of_person = 2003 - current_age_for_birth_year(year)
